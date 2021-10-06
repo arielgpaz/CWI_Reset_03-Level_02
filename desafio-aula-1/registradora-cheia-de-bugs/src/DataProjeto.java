496a2a0c51a6d0d929fd9e8014d37c71f14a4fd9
arielgpaz <seuEmail>
2021-10-06 19:21:38 -0300

    Resolvendo sextoBug. Corrigindo método cozinhaEmFuncionamento para hora >= 6. Corrigindo método CriarDataComCozinhaFuncionando para ser possível gerar hora = 16, estava gerando apenas até 15 h.

--- desafio-aula-1/registradora-cheia-de-bugs/src/DataProjeto.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/DataProjeto.java
@@ -11,7 +11,7 @@ public class DataProjeto {
 
     public static DataProjeto criarDataComCozinhaFuncionando() {
         Random random = new Random();
-        int hora = random.nextInt(10) + 6;
+        int hora = random.nextInt(11) + 6;
         int minuto = hora == 16 ? random.nextInt(40) : random.nextInt(60);
 
         return new DataProjeto(true, hora, minuto);
@@ -40,7 +40,7 @@ public class DataProjeto {
     }
 
     public static boolean cozinhaEmFuncionamento() {
-        boolean isHorarioFuncionamento = hora > 6 && hora <= 16;
+        boolean isHorarioFuncionamento = hora >= 6 && hora <= 16;
         boolean isMinutoFuncionamento = hora == 16 ? minuto <= 40 : minuto < 60;
 
         return diaUtil && isHorarioFuncionamento && isMinutoFuncionamento;
