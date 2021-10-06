7897b286edb26bd0ec50a6906ac3ba89272d2296
arielgpaz <seuEmail>
2021-10-06 19:24:14 -0300

    Resolvendo sextoBug. Criando métodos para "retornarQuantidadeEstoque", "atualizarSaldoEstoque" e avaliar se pussui "estoqueSuficiente".

--- desafio-aula-1/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
+++ desafio-aula-1/registradora-cheia-de-bugs/src/ItensPorQuantidade.java
@@ -1,10 +1,64 @@
 public class ItensPorQuantidade {
 
-    static int pao = 3600;
-    static int torta = 4;
-    static int sanduiche = 20;
-    static int leite = 20;
-    static int cafe = 20;
+    static double pao = 3600;
+    static double torta = 4;
+    static double sanduiche = 20;
+    static double leite = 20;
+    static double cafe = 20;
 
+    public static double retornarQuantidadeEstoque(String item) {
+        if ("pao".equals(item)) {
+            return pao;
+        }
+        if ("sanduiche".equals(item)) {
+            return sanduiche;
+        }
+        if ("torta".equals(item)) {
+            return torta;
+        }
+        if ("leite".equals(item)) {
+            return leite;
+        }
+        if ("cafe".equals(item)) {
+            return cafe;
+        }
+        return 0;
+    }
 
+    public static void atualizarSaldoEstoque (String item, double quantidade) {
+        if ("pao".equals(item)) {
+            pao -= quantidade * 60;
+        }
+        if ("sanduiche".equals(item)) {
+            sanduiche -= quantidade;
+        }
+        if ("torta".equals(item)) {
+            torta -= quantidade / 16;
+        }
+        if ("leite".equals(item)) {
+            leite -= quantidade;
+        }
+        if ("cafe".equals(item)) {
+            cafe -= quantidade;
+        }
+    }
+
+    public static boolean estoqueSuficiente(String item, double quantidade) {
+        if ("pao".equals(item)) {
+            return quantidade <= pao;
+        }
+        if ("sanduiche".equals(item)) {
+            return quantidade <= sanduiche;
+        }
+        if ("torta".equals(item)) {
+            return quantidade <= torta * 16;
+        }
+        if ("leite".equals(item)) {
+            return quantidade <= leite;
+        }
+        if ("cafe".equals(item)) {
+            return quantidade <= cafe;
+        }
+        return false;
+    }
 }
